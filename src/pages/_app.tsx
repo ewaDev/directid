c25dfa474284fe78e02137682764c14e239a2b80
Ewa G <e.grabowiecka@outlook.com>
2023-07-31 22:55:52 +0100

    feat: fixing page props and removing unecessary div

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <div className="main">
         <Component {...pageProps} />
-      </div>
   );
 }
 export default MyApp;
