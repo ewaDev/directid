4ae76ed15f2c8921bd1069a19bb49013e0b366c0
Ewa G <e.grabowiecka@outlook.com>
2023-07-31 23:37:47 +0100

    feat: adding env variables for deployment

--- src/pages/api/accounts.ts
+++ src/pages/api/accounts.ts
@@ -1,7 +1,6 @@
-
 export async function getAccountData (){
-    console.log("getAccountData")
-   return fetch("https://directid.vercel.app/apollo-carter.json")
+   const baseUrl = process.env["BASE_URL"]
+   return fetch(`${baseUrl}/apollo-carter.json`)
         .then(response => response.json())
         .catch(error => console.log('error', error));
 }
