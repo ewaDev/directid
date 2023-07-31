8a9c7477caab6b9d4cb8e6df65e7f5cf9ae954a0
Ewa G <e.grabowiecka@outlook.com>
2023-07-31 22:58:27 +0100

    feat: fixing app tsx page props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import React from "react";
-
-function MyApp({ Component, pageProps }) {
+import type { AppProps } from "next/app";
+function MyApp({ Component, pageProps }: AppProps) {
   return (
       <div className="main">
         <Component {...pageProps} />
