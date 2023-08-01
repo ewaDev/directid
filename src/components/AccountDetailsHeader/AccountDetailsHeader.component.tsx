4e80a94897dbb9bb86fd52a5ad108021ede6135b
Ewa G <e.grabowiecka@outlook.com>
2023-08-01 22:54:02 +0100

    feat: fixing styling

--- src/components/AccountDetailsHeader/AccountDetailsHeader.component.tsx
+++ src/components/AccountDetailsHeader/AccountDetailsHeader.component.tsx
@@ -32,14 +32,14 @@ export default function AccountDetailsHeader({accountHolderNames, availableBalan
                     <h4 className={"dark-font bold"}> User Transactions ({currencyCode}) </h4>
                 </div>
 
-                <div className={"lg:place-self-end mr-1"}>
-                    <div className="flex items-end bg-blue-100 p-6 gap-4 rounded-xl max-w-80 justify-between">
+                <div className={"xl:place-self-end mr-1"}>
+                    <div className="flex items-end bg-blue-100 p-6 lg:gap-12 gap-8 rounded-xl max-w-80 justify-between">
                         <div className="flex flex-col items-start">
                             <h5 className="dark-font bg-blue-100 bold">{accountHolderNames}</h5>
                             <p>Account number: {accountNumber}</p>
                             <p>Branch Code: {formatBranchCode(bankCode)}</p>
                         </div>
-                        <div className="flex flex-col items-end">
+                        <div className="flex flex-col items-end ">
                             <h4 className="dark-font">Balance</h4>
                             <h1 className={"pt-1"}>{availableBalance}</h1>
                         </div>
